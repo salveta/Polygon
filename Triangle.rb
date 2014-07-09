6436a247a811068e0686833be2ccbdc9e2ba365b
salveta <salveta4_@hotmail.com>
2014-07-09 15:46:01 +0200

    Failure/Error: expect{Triangle.new()}.to raise_error(ArgumentError) GREEN

--- Triangle.rb
+++ Triangle.rb
@@ -2,9 +2,18 @@ require './Polygon'
 
 class Triangle < Polygon
 
+ def initialize(*sides)
+    number_sides
+    need_three_sides (sides)
+  end
 
   def number_sides
-    3
+    @number_of_sides = 3
+    end
+
+  def need_three_sides (sides)
+    raise ArgumentError.new("You need three sides to be a Triangle") if sides.size < @number_of_sides
+    raise ArgumentError.new("You need three sides to be a Triangle") if sides.size > @number_of_sides
   end
 
 end
--- spec/poligon_spec.rb
+++ spec/poligon_spec.rb
@@ -6,15 +6,17 @@ describe "A Polygon" do
   expect(Triangle.new(1,2,3).number_sides).to eq(3)
   end	
 
-  it "Is a polygon" do
-  expect(Triangle.new(1,2.3).class.ancestors).to include(Polygon)
-  end
+  #it "Is a polygon" do
+  #expect(Triangle.new(1,2.3).class.ancestors).to include(Polygon)
+  #end
 
-  it "needs three sides to be a Triangle" do
+  it "Three sides to be a Triangle" do
     expect{Triangle.new()}.to raise_error(ArgumentError)
     expect{Triangle.new(1)}.to raise_error(ArgumentError)
     expect{Triangle.new(1,2)}.to raise_error(ArgumentError)
     expect{Triangle.new(1,2,3,4)}.to raise_error(ArgumentError)
   end
-   
+
+
+
 end
