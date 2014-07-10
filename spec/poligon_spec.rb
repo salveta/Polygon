1e93659558046945de068091015b684f64007f9d
salveta <salveta4_@hotmail.com>
2014-07-10 11:46:07 +0200

    All sides have to be positive and no words GREEN

--- spec/poligon_spec.rb
+++ spec/poligon_spec.rb
@@ -39,12 +39,18 @@ end
 
 describe "A Square" do
 
-  it "Number os sides is four" do
+  it "Number of sides is four" do
     expect(Square.new(1,2,3,4).number_sides).to eq(4)
   end
 
   it "is also a Polygon" do
     expect(Square.new(2,2,3).class.ancestors).to include(Polygon)
   end
+
+  it "All sides have to be positive and no words" do
+    expect{Triangle.new(1,2,"hola")}.to raise_error(ArgumentError)
+    expect{Triangle.new(3,-5,4)}.to raise_error(ArgumentError)
+  end
+
 
 end
